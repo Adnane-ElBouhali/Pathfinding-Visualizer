760c58d4b8d1effc78cf19fd36c0b4faf8523ce9
Ayoub Marzoug <92301593+ayoub-mg@users.noreply.github.com>
2022-01-30 23:59:52 +0100

    Update dijkstra.js
    Ajout de la méthode qui trace le chemin le plus court à partir du dernier noeud.

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -42,4 +42,13 @@ function getAllNodes(grid) {
   }
   return nodes;
 }
-  
+
+export function getNodesInShortestPathOrder(finishNode) {
+  const nodesInShortestPathOrder = [];
+  let currentNode = finishNode;
+  while (currentNode !== null) {
+    nodesInShortestPathOrder.unshift(currentNode);
+    currentNode = currentNode.previousNode;
+  }
+  return nodesInShortestPathOrder;
+}  
