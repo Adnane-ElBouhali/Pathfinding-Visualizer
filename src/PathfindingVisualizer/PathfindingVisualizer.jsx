1c3b02e47177732067be62ee98216dc513a96b35
Adnane-ElBouhali <96652034+Adnane-ElBouhali@users.noreply.github.com>
2022-01-31 23:02:27 +0100

    Update PathfindingVisualizer.jsx
    On ajoute une méthode qui permet d'animer le plus court chemin.

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -37,4 +37,14 @@ export default class PathfindingVisualizer extends Component {
     this.setState({ mouseIsPressed: false });
   }
   
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-shortest-path';
+      }, 50 * i);
+    }
+  }
+  
 }
